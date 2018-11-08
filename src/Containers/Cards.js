5ab5ff06459fc2b37013d2a24d931fea22beaf8f
github username <personabe1984@gmail.com>
2018-11-07 19:31:50 -0500

    Creating the backbone of the project with react Component

---
+++ src/Containers/Cards.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Cards extends Component {
+  render(){
+    return (
+      <div>Cards
+      CardsCardsCardsCards
+      <h1>
+       <h1>      CardsCardsCardsCards
+</h1>
+       <h1>      CardsCardsCardsCards
+</h1>
+      </h1>
+
+      </div>
+    )
+  }
+}
+
+export default Cards;
---
+++ src/Containers/ScoresHeader.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ScoresHeader extends Component {
+
+  render(){
+    return(
+      <div> Score Header</div>
+    )
+  }
+}
+
+export default ScoresHeader;
